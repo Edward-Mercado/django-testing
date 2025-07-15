435ce9dc5046d8a5d23eb15df4db08c7cdeb7deb
Edward-Mercado <edwardmercado149@gmail.com>
2025-07-15 09:55:37 -0400

    wahhhhhhhhhhhhhhh

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
+    path('', views.main, name='main'),
+    path('testing/', views.testing, name='testing'),
 ]
